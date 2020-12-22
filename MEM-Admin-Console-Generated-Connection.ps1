0e9fb9581568d1a4b250aaf904efa2f4aed21db7
Mike Bishop <76496950+BishopCloud@users.noreply.github.com>
2020-12-22 15:29:19 -0600

    Create MEM-Admin-Console-Generated-Connection.ps1

---
+++ MEM-Admin-Console-Generated-Connection.ps1
@@ -0,0 +1,21 @@
+# Site configuration
+$SiteCode = "EM1" # Site code 
+$ProviderMachineName = "FMM31597MEM001.backyard.corp" # SMS Provider machine name
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
